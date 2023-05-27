6c7d8d89e163490d038baada6f7e973b942b643d
Francesco Pio Cellamare <s309530@studenti.polito.it>
2023-05-27 16:19:27 +0200

    added next and best

--- index.js
+++ index.js
@@ -5,39 +5,90 @@ import TelegramBot from "node-telegram-bot-api"
 
 const addr = "https://www.swas.polito.it/dotnet/orari_lezione_pub/RicercaAuleLiberePerFasceOrarie.aspx"
 const BOT_TOKEN = '6101207167:AAHh_YywGPOqmqPXWBPbkNKgkAcj69U8Cbk';
+const setOfRooms = ["1", "10", "10A", "10C", "10D", "10I", "11", "11B", "11I", "11S", "11T", "12", "12A", "12D", "12I", "13", "13A", "13B", "13S", "14", "15", "15A", "16", "17", "17A", "19", "19A", "1B", "1I", "1M", "1P", "1S", "2", "21A", "27", "27B", "29", "29B", "2C", "2D", "2I", "2M", "2N", "2P", "3", "3I", "3M", "3N", "3P", "3S", "4", "4C", "4D", "4I", "4M", "4N", "4P", "4T", "5", "5B", "5I", "5M", "5N", "5S", "6", "6C", "6D", "6I", "6N", "7", "7B", "7I", "7N", "7S", "7T", "8", "8C", "8D", "8I", "9B", "9I", "9S", "9T", "R1", "R1b", "R2", "R2b", "R3", "R3b", "R4", "R4b"]
+const noSlot = 8
+
+const bot = new TelegramBot(BOT_TOKEN, { polling: true })
 
 function sendMessageToBot(id, availableList) {
 
-    const messageText = `Sono disponibili ${availableList}`;
+    const messageText = availableList.length === 0 ? 'No rooms' : availableList.join(', ');
 
     axios.post(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
         chat_id: id,
         text: messageText,
     })
         .then(response => {
-            console.log('Message sent successfully:', response.data.result.text);
+            console.log('Message sent successfully to :', id);
         })
         .catch(error => {
-            console.error('Error sending message:', error.response.data);
+            console.error('Error sending message:', error);
         });
 }
 
 
-async function fetchOrari(msg) {
+async function fetchOrari() {
     const response = await axios.get(addr);
     const html = response.data;
-    const $ = cheerio.load(html);
-    const $selected = $('[id=Pagina_gv_AuleLibere_lbl_AuleLibere_0]').text()
-    sendMessageToBot(msg.chat.id, $selected);
+    return html
+}
 
+function getRooms(html, index) {
+    const $ = cheerio.load(html);
+    const $selected = $(`[id=Pagina_gv_AuleLibere_lbl_AuleLibere_${index}]`)
+                            .text().split(', ').map(room => room.trim())
     return $selected
 }
 
 
-
-const bot = new TelegramBot(BOT_TOKEN, { polling: true })
-
-bot.onText(new RegExp('\/ora'), (msg) => {
-    const queryResult = fetchOrari(msg)
+bot.onText(new RegExp('\/now'), async (msg) => {
+    const index = 0
+    const html = await fetchOrari()
+    const queryResult = getRooms(html, index)
+    sendMessageToBot(msg.chat.id, queryResult);
 })
 
+bot.onText(new RegExp('\/next'), async (msg) => {
+    const index = 1
+    const html = await fetchOrari()
+    const queryResult = getRooms(html, index)
+    sendMessageToBot(msg.chat.id, queryResult);
+})
+
+bot.onText(new RegExp('\/best'), async (msg) => {
+    let bitmap = {}
+    let slots = {}
+    for(let room of setOfRooms) {
+        bitmap[room] = []
+        slots[room] = 0
+    }
+    
+    const html = await fetchOrari()
+    for(let i = 0; i < noSlot; i++) {
+        let currentAvailable = getRooms(html, i)
+        for(let room in bitmap) {
+            if(currentAvailable.includes(room))
+                bitmap[room].push(1)
+            else
+                bitmap[room].push(0)
+        }
+    }
+    for(let room in slots) {
+        slots[room] = maxAvail(0, bitmap[room])
+    }
+    const maxFree = Math.max(...Object.values(slots))
+    let result = []
+    for(let room in slots) {
+        if(slots[room] == maxFree)
+            result.push(room)
+    }
+    
+    sendMessageToBot(msg.chat.id, result);
+})
+
+function maxAvail(pos, availability){
+    let i = pos
+    while (availability[i]) 
+        i++
+    return i
+}
